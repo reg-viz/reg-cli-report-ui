4f77b804ebf1dab8a2c005adc8fcca1e7376cfaa
wadackel <wadackel@gmail.com>
2024-04-21 16:03:02 +0900

    refactor: avoid unnecessary renders

--- src/components/internal/Collapse/Collapse.tsx
+++ src/components/internal/Collapse/Collapse.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useRef } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import * as styles from './Collapse.css';
 
 export type Props = Omit<
@@ -17,12 +17,10 @@ export const Collapse = ({ open, duration, children, ...rest }: Props) => {
     const { current: body } = bodyRef;
 
     node.style.height = `${body != null ? body.clientHeight : 0}px`;
-    node.style.overflow = 'hidden';
   }, []);
 
   const clearInlineStyles = useCallback((node: HTMLElement) => {
     node.style.height = '';
-    node.style.overflow = '';
   }, []);
 
   const handleEnter = useCallback(
@@ -54,30 +52,34 @@ export const Collapse = ({ open, duration, children, ...rest }: Props) => {
   );
 
   return (
-    <CSSTransition
-      classNames="collapse"
-      in={open}
-      timeout={duration}
-      onEnter={handleEnter}
-      onEntered={handleEntered}
-      onExit={handleExit}
-      onExiting={handleExiting}
-    >
-      <div
-        {...rest}
-        className={styles.wrapper}
-        style={
-          {
-            '--collapse-duration-enter': `${duration.enter}ms`,
-            '--collapse-duration-exit': `${duration.exit}ms`,
-          } as React.CSSProperties
-        }
-        aria-hidden={open ? 'false' : 'true'}
-      >
-        <div className={styles.inner} ref={bodyRef}>
-          <div className={styles.innerBox}>{children}</div>
-        </div>
-      </div>
-    </CSSTransition>
+    <TransitionGroup>
+      {open && (
+        <CSSTransition
+          key="collapse"
+          in={open}
+          classNames="collapse"
+          timeout={duration}
+          onEnter={handleEnter}
+          onEntered={handleEntered}
+          onExit={handleExit}
+          onExiting={handleExiting}
+        >
+          <div
+            {...rest}
+            className={styles.wrapper}
+            style={
+              {
+                '--collapse-duration-enter': `${duration.enter}ms`,
+                '--collapse-duration-exit': `${duration.exit}ms`,
+              } as React.CSSProperties
+            }
+          >
+            <div className={styles.inner} ref={bodyRef}>
+              <div className={styles.innerBox}>{children}</div>
+            </div>
+          </div>
+        </CSSTransition>
+      )}
+    </TransitionGroup>
   );
 };
